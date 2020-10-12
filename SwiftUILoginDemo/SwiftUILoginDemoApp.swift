90eb19fa669b48fb00bc90307ca27f21a6d5d3fb
RajendranEshwaran <rajendranmca2010@gmail.com>
2020-10-12 13:58:21 -0400

    Initial Commit

---
+++ SwiftUILoginDemo/SwiftUILoginDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILoginDemoApp.swift
+//  SwiftUILoginDemo
+//
+//  Created by gomathi saminathan on 10/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILoginDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
